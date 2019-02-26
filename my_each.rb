b7d4e7e6059b559932b6d71f69ae5a27b40a8e62
A Belgrave <abelgrave95@gmail.com>
2019-02-26 17:48:47 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -4,8 +4,8 @@ def my_each(array) # put argument(s) here
       i = 0
 
       while i < array.length
-        yield(array[i])
         puts array[i]
+        yield(array[i])
         i = i + 1
       end
 
